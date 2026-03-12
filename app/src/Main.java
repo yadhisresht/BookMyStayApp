85a4b0c3799e9f9729feed3ea11eba5d1a5befc6
YADHISRESHT HARIKRISHNAN <152004898+yadhisresht@users.noreply.github.com>
2026-03-12 08:40:09 +0530

    Add files via upload

---
+++ app/src/Main.java
@@ -0,0 +1,34 @@
+/**
+ * Hotel Booking Application
+ *
+ * This class represents the entry point of the Hotel Booking System.
+ * It demonstrates how a Java application starts execution using
+ * the main() method and prints a welcome message to the console.
+ *
+ * @author laksh
+ * @version UC1
+ */
+
+public class UseCase1HotelBookingApp {
+
+    /**
+     * Main method – Entry point of the Java application.
+     * The JVM starts execution from this method.
+     *
+     * @param args command-line arguments
+     */
+    public static void main(String[] args) {
+
+        // Printing application welcome message
+        System.out.println("=======================================");
+        System.out.println("        HOTEL BOOKING SYSTEM           ");
+        System.out.println("=======================================");
+        System.out.println("Application Name : Book My Stay App");
+        System.out.println("Version          : vUC1");
+        System.out.println("---------------------------------------");
+        System.out.println("Welcome to the Hotel Booking System!");
+        System.out.println("Your comfortable stay begins here.");
+        System.out.println("=======================================");
+
+    }
+}
